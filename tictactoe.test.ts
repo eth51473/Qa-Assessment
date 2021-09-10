924fa1ed5b8a69146ed0de5690302339216a5b7d
Ethan Bird <eth51473@gmail.com>
2021-09-10 12:47:02 -0600

    finished Lab W/o Extra Credit

--- tictactoe.test.ts
+++ tictactoe.test.ts
@@ -18,3 +18,36 @@ test('I can start a game', async () => {
     await button.click();
     
 });
+test('I can place an x in the upper right square', async () => {
+
+    // let button = await (await driver).findElement(By.id('start-game'));
+    // await button.click();
+    let cell = await driver.findElement(By.id('cell-0'))
+    cell.click()
+    let cellText = await driver.findElement(By.id('cell-0')).getText()
+    expect(cellText).toBe('X')
+    await driver.sleep(2000)
+    
+});
+test('The computer will place an o after an x is placed', async () => {
+
+    // let button = await (await driver).findElement(By.id('start-game'));
+    // await button.click();
+    let cell = await driver.findElement(By.id('cell-2'))
+    cell.click()
+    let cellText = await driver.findElement(By.id('cell-3')).getText()
+    expect(cellText).toBe('o')
+    await driver.sleep(2000)
+    
+});
+test('I can place an x in the lower right square', async () => {
+
+    // let button = await (await driver).findElement(By.id('start-game'));
+    // await button.click();
+    let cell = await driver.findElement(By.id('cell-8'))
+    cell.click()
+    let cellText = await driver.findElement(By.id('cell-8')).getText()
+    expect(cellText).toBe('X')
+    await driver.sleep(2000)
+    
+});
